c8a9e171c6bb8cd3dea916fc54de874c3809fbc1
bhavikajain8-coder <bhavikabrahmadande1@gmail.com>
2025-09-24 00:46:58 +0530

    Added VS Code project files

---
+++ One-Cart-Food-Application/src/main/java/in/bhavikaproject/One_Cart_Food_Application/OneCartFoodApplication.java
@@ -0,0 +1,13 @@
+package in.bhavikaproject.One_Cart_Food_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCartFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCartFoodApplication.class, args);
+	}
+
+}
---
+++ One-Cart-Food-Application/src/test/java/in/bhavikaproject/One_Cart_Food_Application/OneCartFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bhavikaproject.One_Cart_Food_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCartFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
